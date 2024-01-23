97d40145965029abbb10ba19f3f6d3cb93d46f0c
Huaqi Fang <578567190@qq.com>
2024-01-23 19:16:18 +0800

    add NOLIB=0 to build for c nano library
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- build_m4.sh
+++ build_m4.sh
@@ -1,13 +1,25 @@
 ARCH_OPT=${ARCH_OPT:-"-mcpu=cortex-m4"}
 OLEVEL=${OLEVEL:-"-Os"}
+NOLIB=${NOLIB:-1}
+
+LIBFLAGS=" -nostdlib"
+USERLIBS=""
+DUMMYLIBS="crt0 libgcc libm libc"
+if [ "x$NOLIB" != "x1" ] ; then
+    LIBFLAGS="-specs=nosys.specs -specs=nano.specs"
+    USERLIBS="-lm"
+    DUMMYLIBS=""
+fi
 
 set -x
+
 ./build_all.py --clean --arch arm --chip cortex-m4 --board generic \
     --cc arm-none-eabi-gcc \
     --cflags="-c ${OLEVEL} ${ARCH_OPT} -ffunction-sections" \
-    --ldflags="${ARCH_OPT} -nostdlib -Wl,-gc-sections" \
-    --user-libs="" \
-    --dummy-libs="crt0 libgcc libm libc"
+    --ldflags="${ARCH_OPT} ${LIBFLAGS} -Wl,-gc-sections" \
+    --user-libs="$USERLIBS" \
+    --dummy-libs="$DUMMYLIBS"
+
 set +x
 
 ./benchmark_size.py
--- build_nuclei.sh
+++ build_nuclei.sh
@@ -1,13 +1,24 @@
 ARCH_OPT=${ARCH_OPT:-"-march=rv32imafd_zca_zcb_zcf_zcmp_zcmt_zba_zbb_zbc_zbs_zicond_xxlcz -mabi=ilp32d"}
 OLEVEL=${OLEVEL:-"-Os"}
+NOLIB=${NOLIB:-1}
+
+LIBFLAGS=" -nostdlib"
+USERLIBS=""
+DUMMYLIBS="crt0 libgcc libm libc"
+if [ "x$NOLIB" != "x1" ] ; then
+    LIBFLAGS="-specs=nosys.specs -specs=nano.specs"
+    USERLIBS="-lm"
+    DUMMYLIBS=""
+fi
 
 set -x
 ./build_all.py --clean --arch riscv32 --chip generic --board ri5cyverilator \
     --cc riscv64-unknown-elf-gcc \
     --cflags="-c ${OLEVEL} ${ARCH_OPT} -ffunction-sections" \
-    --ldflags="${ARCH_OPT} -nostdlib -Wl,-gc-sections" \
-    --user-libs="" \
-    --dummy-libs="crt0 libgcc libm libc"
+    --ldflags="${ARCH_OPT} ${LIBFLAGS} -Wl,-gc-sections" \
+    --user-libs="$USERLIBS" \
+    --dummy-libs="$DUMMYLIBS"
+
 set +x
 
 ./benchmark_size.py
